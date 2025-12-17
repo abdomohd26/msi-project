bde11d8e57d66b33e4ab6e64f451d245becbc09b
Salmanahmed22 <123300970+Salmanahmed22@users.noreply.github.com>
2025-12-17 16:08:57 +0200

    separate knn in our architecture

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 class_weights = {0:1.0, 1:1.0, 2:1.2, 3:1.5, 4:1.5, 5:2.0} 
 SVM_CONFIG = {
-    "kernel": "rbf",       # linear / rbf / poly
+    "kernel": "rbf",
     "C": 10.0,            # regularization parameter
     "gamma": "scale",
     "probability": True, 
@@ -8,3 +8,9 @@ SVM_CONFIG = {
 }
 
 UNKNOWN_THRESHOLD = 0.6   # confidence threshold
+
+KNN_CONFIG = {
+    "n_neighbors": 9,
+    "weights": "distance",
+    "metric": "euclidean"
+}
